269a7f9f350aee1709b15d0cc4b41c26b6b338e2
YunJin Choi <zzdd1558@gmail.com>
2020-05-07 22:49:21 +0900

    프록시

--- src/main/java/com/superdev/jpa/DemoApplication.java
+++ src/main/java/com/superdev/jpa/DemoApplication.java
@@ -26,18 +26,25 @@ public class DemoApplication {
                     .team(team)
                     .build();
 
+            Member member1 = Member.builder()
+                    .username("원빈")
+                    .team(team)
+                    .build();
+
             em.persist(team);
             em.persist(member);
-            tx.commit();//트랜잭션 커밋
+            em.persist(member1);
 
+            em.flush();
             em.clear();
 
 
-            tx.begin(); //트랜잭션 시작
-            printUser(em, member.getId());
-//            printUserAndTeam(em, member.getId());
+            //printUser(em, member.getId());
+            //printUserAndTeam(em, member.getId());
+            //proxyEqualsClass(em);
+            //doesItExistInTheContextOfPersistenceExam1(em);
+            doesItExistInTheContextOfPersistenceExam2(em);
             tx.commit();//트랜잭션 커밋
-
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback(); //트랜잭션 롤백
@@ -67,5 +74,43 @@ public class DemoApplication {
         System.out.println("객체의 이름 : " + findMember.getClass().getName());
         System.out.println("회원 이름 : " + findMember.getUsername());
         System.out.println("-----printUser END------");
+    }
+
+    public static void proxyEqualsClass (EntityManager em ) {
+        Member member = em.find(Member.class , 2L);
+        Member memberProxy = em.getReference(Member.class , 3L);
+
+        System.out.println("-----proxyEqualsClass START------");
+        System.out.println("member find className :: " + member.getClass().getName());
+        System.out.println("memberProxy getReference className :: " + memberProxy.getClass().getName());
+        System.out.println("find == getReference :: " + (member == memberProxy));
+        System.out.println("find member is Member.class??  :: " + (member instanceof  Member));
+        System.out.println("getReference memberProxy is Member.class??  :: " + (memberProxy instanceof  Member));
+        System.out.println("-----proxyEqualsClass END------");
+    }
+
+    public static void doesItExistInTheContextOfPersistenceExam1(EntityManager em) {
+        Member member = em.find(Member.class , 2L);
+        Member memberProxy = em.getReference(Member.class , 2L);
+
+        System.out.println("-----doesItExistInTheContextOfPersistence START------");
+        System.out.println("member find className :: " + member.getClass().getName());                      //com.superdev.jpa.proxy.entity.Member
+        System.out.println("memberProxy getReference className :: " + memberProxy.getClass().getName());    //com.superdev.jpa.proxy.entity.Member
+        System.out.println(member == memberProxy);
+        System.out.println("-----doesItExistInTheContextOfPersistence START------");
+    }
+
+
+    public static void doesItExistInTheContextOfPersistenceExam2(EntityManager em) {
+        Member memberProxy = em.getReference(Member.class , 2L);
+        Member member = em.find(Member.class , 2L);
+        System.out.println("-----doesItExistInTheContextOfPersistence START------");
+        System.out.println("member find className :: " + member.getClass().getName());                      //com.superdev.jpa.proxy.entity.Member
+        System.out.println("memberProxy getReference className :: " + memberProxy.getClass().getName());    //com.superdev.jpa.proxy.entity.Member
+
+        // 두 객체는 비록 참조값은 다르지만 같다를 보장해줘야 한다.
+        // Proxy든 아니든 개발에 문제가 없도록 true를 보장해줘야 한다 .
+        System.out.println(member == memberProxy);
+        System.out.println("-----doesItExistInTheContextOfPersistence START------");
     }
 }
