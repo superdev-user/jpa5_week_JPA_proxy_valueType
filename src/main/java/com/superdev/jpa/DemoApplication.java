0dda9fa66b6607489c5d1be937873c42bd853ba8
yunjin choi <zzdd1558@naver.com>
2020-05-09 20:35:13 +0900

    값 타입 컬렉션 사용 방법 추가중

--- src/main/java/com/superdev/jpa/DemoApplication.java
+++ src/main/java/com/superdev/jpa/DemoApplication.java
@@ -19,16 +19,15 @@ public class DemoApplication {
         try {
             tx.begin(); //트랜잭션 시작
 
-            List<Address> addressList = new ArrayList<>(Arrays.asList(
-                    Address.builder().city("경기도").street("오산시").zipcode("08580").build(),
-                    Address.builder().city("서울시").street("관악구").zipcode("08580").build()
-            ));
-
             Member member = Member.builder()
                     .name("윤진")
                     .age(28)
-                    .address(addressList)
+                    .address(new ArrayList<Address>())
                     .build();
+
+            member.getAddress().add(Address.builder().city("경기도").street("오산시").zipcode("08580").build());
+            member.getAddress().add(Address.builder().city("서울시").street("관악구").zipcode("08580").build());
+
 
             em.persist(member);
             tx.commit();//트랜잭션 커밋
--- src/main/java/com/superdev/jpa/proxy/entity/Member.java
+++ src/main/java/com/superdev/jpa/proxy/entity/Member.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
